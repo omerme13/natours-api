564e52dc80d11a2dd5a06813b682ab22f412dff1
omerme13 <omerme13@gmail.com>
2020-02-03 18:26:54 +0200

    set mongoose with mongoDB atlas

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const { DATABASE, DATABASE_PASSWORD } = process.env;
+const db = DATABASE.replace('<PASSWORD>', DATABASE_PASSWORD);
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('connection successful!'))
 
 
 const port = process.env.PORT || 3000;
