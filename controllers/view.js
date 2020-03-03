97b8e95e6791752fa9f1ffdd8ad4198e0973755c
omerme13 <omerme13@gmai.com>
2020-03-03 13:35:31 +0200

    tour detail page, fixed bugs

--- controllers/view.js
+++ controllers/view.js
@@ -1,11 +1,17 @@
 const Tour = require('../models/tour');
 const catchAsync = require('../utils/catchAsync');
 
-const getTour = (req, res) => {
-    res.render('tour', {
-        title: 'The Forest Hiker',
+const getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({ slug: req.params.slug}).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     });
-}
+
+    res.render('tour', {
+        title: tour.name,
+        tour
+    });
+});
 
 const getOverview = catchAsync(async (req, res, next) => {
     const tours = await Tour.find();
--- routers/view.js
+++ routers/view.js
@@ -4,7 +4,7 @@ const viewController = require('../controllers/view');
 
 const router = express.Router();
 
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 router.get('/', viewController.getOverview);
 
 module.exports = router;    
